ae79cd645ae293bf3cfbfd78eb003df7fed6c95a
Alejandro Medina <alejandro@radishlab.com>
2020-11-18 17:57:56 -0300

    1.4 Set parts objects in array

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,9 +10,7 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.part1.name} number={props.part1.exercises} />
-      <Part name={props.part2.name} number={props.part2.exercises} />
-      <Part name={props.part3.name} number={props.part3.exercises} />
+      {props.parts.map(part => <Part name={part.name} number={part.exercises} />)}
     </div>
   )
 }
@@ -33,24 +31,32 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  let total = 0
+
+  parts.forEach(part => {
+    total += part.exercises
+  })
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total total={total} />
     </div>
   )
 }
