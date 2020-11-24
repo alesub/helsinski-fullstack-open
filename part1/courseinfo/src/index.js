2cc55ceff44effe29fe176c81629b8dc55bc1780
Alejandro Medina <alejandro@radishlab.com>
2020-11-24 09:23:14 -0300

    1.5 Refactor course data into a single object

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom'
 
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
 const Content = (props) => {
   return (
     <div>
-      {props.parts.map(part => <Part name={part.name} number={part.exercises} />)}
+      {props.course.parts.map(part => <Part name={part.name} number={part.exercises} />)}
     </div>
   )
 }
@@ -24,39 +24,41 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-  return (
-    <p>Number of exercises {props.total}</p>
-  )
-}
-
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
   let total = 0
 
-  parts.forEach(part => {
+  props.course.parts.forEach(part => {
     total += part.exercises
   })
 
   return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total total={total} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
